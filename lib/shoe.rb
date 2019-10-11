a2904359e82c63a8c38dc31366aeb3d412208c65
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-11 12:54:57 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
     BRANDS << brand
-    BRANDS = BRANDS.uniq
-    # if !BRANDS.include?(brand)
-    # BRANDS << brand
-    # end
+    end
   end
 
   def cobble
